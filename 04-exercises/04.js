c3c4c8c5380596940f30dfa1d0c4cfcbd6c9865c
Jorge Ballester <jorge.ballcor@gmail.com>
2025-05-12 17:41:09 +0200

    feat: ex 5

--- 04-exercises/04.js
+++ 04-exercises/04.js
@@ -2,7 +2,7 @@
 
 let i = 0;
 
-while (i <= 10) {
+while (i < 10) {
 
     if (i%2 !== 0) {
         console.log('Odd: ', i)
---
+++ 04-exercises/05.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * Create an algorithm to return the
+ * biggest and smallest number of an array
+ */
+
+let array = [2, 5, 7, 15, -7, -100, 56]
+
+function getMinorMajor(arr) {
+
+    let minor = 0;
+    let major = 0;
+
+    for (let number of arr) {
+
+        if (number < minor) {
+            minor = number;
+        } else if (number > major) {
+            major = number
+        }
+    }
+
+
+    return [minor, major]
+
+}
+
+let numbers = getMinorMajor(array)
+console.log(numbers)

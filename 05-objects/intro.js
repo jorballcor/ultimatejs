63f936ac57af6a22bef30a0858872f0830e11d55
Jorge Ballester <jorge.ballcor@gmail.com>
2025-05-13 11:27:52 +0200

    feat: OOP in JS

---
+++ 05-objects/intro.js
@@ -0,0 +1,13 @@
+let user = {
+    email: 'jorge@helloworld.io',
+    name: 'Jorge',
+    adress: {
+        street: 'Queen St',
+        number: 14
+    },
+    active: true,
+    retrieveKey: function () { //Anonymous function
+        console.log('Retrieving key...');
+    }, 
+    
+}

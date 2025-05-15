e79229f466bbc1f0761873fa0f4c4fdcb23fbfe1
Jorge Ballester <jorge.ballcor@gmail.com>
2025-05-15 11:08:25 +0200

    feat: scape

---
+++ 05-objects/15-scape.js
@@ -0,0 +1,10 @@
+const greet = "Hello \\world!\welcome to\t \"ultimate javascript\" :)"
+console.log(greet); // Hello \world!\welcome to\t "ultimate javascript" :)
+
+/**
+ * \n nueva linea
+ * \t tabulación
+ * \' comilla simple
+ * \" comilla doble
+ * \\ backslash
+ */
